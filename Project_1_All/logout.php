6b0ad19f7743c39f883328b1b5bbf85ac0130518
CyrusWolf <73084724+ArciusWolf@users.noreply.github.com>
2023-08-15 09:41:39 +0700

    Delete Project_1_All directory

--- Project_1_All/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    $_SESSION["fullName"] = "";
-
-    header("Location: index_home.php");
--- Project_1_All/register_success.php
+++
@@ -1,2 +0,0 @@
-<h3>Đăng ký tài khoản thành công</h3>
-<p><a href="index_home.php">Trang Chủ</a></p>
